001956c16af2051b4b780f9e620df10238698180
Muhammad S. Maromi <msmaromi@gmail.com>
2013-05-16 22:05:50 +0700

    waktu dunia poke gan

---
+++ Pokeranch2/src/com/pokeranch/model/PokeTime.java
@@ -0,0 +1,69 @@
+package com.pokeranch.model;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.util.Log;
+
+public class PokeTime {	
+	public final int ONE_SECOND = 1000;		//dalam ms
+	public final int POKE_HALF_DAY = 60;	//dalam s
+	
+	private int pokeSecond;
+	private int pokeDay;
+	private Timer timer;
+	
+	public PokeTime() {
+		pokeSecond = 0;
+		pokeDay = 0;
+		timer = new Timer();
+	}
+	
+	public PokeTime(int second, int day, Timer t) {
+		pokeSecond = second;
+		pokeDay = day;
+		timer = t;
+	}
+	
+	public int getPokeSecond() {
+		return pokeSecond;
+	}
+	
+	public void setPokeSecond(int newPokeSecond) {
+		pokeSecond = newPokeSecond;
+	}
+	
+	public int getPokeDay() {
+		return pokeDay;
+	}
+	
+	public void setPokeDay(int newPokeDay) {
+		pokeDay = newPokeDay;
+	}
+	
+	public void start() {
+		timer.scheduleAtFixedRate(new TimerTask() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub				
+				if(pokeSecond>2*POKE_HALF_DAY) {
+					pokeDay++;
+					pokeSecond = 1;					
+				}
+				pokeSecond++;
+				Log.d("second ke", String.valueOf(pokeSecond));
+				Log.d("day ke", String.valueOf(pokeDay));
+			}
+		}, ONE_SECOND, ONE_SECOND);
+	}
+	
+	public void pause() {
+		timer.cancel();
+	}
+	
+	public void stop() {
+		timer.cancel();
+		timer.purge();
+	}
+}
